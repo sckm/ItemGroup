0b283a8d4a9106eda172bd84b3e82279f25b2f10
scache <scal.che@gmail.com>
2019-12-09 02:23:51 +0900

    rename benchmark class

--- benchmark/src/androidTest/java/com/github/sckm/itemgroup/benchmark/ItemGroupBenchmark.kt
+++ benchmark/src/androidTest/java/com/github/sckm/itemgroup/benchmark/ItemGroupBenchmark.kt
@@ -13,7 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class BenchmarkSample {
+class ItemGroupBenchmark {
     @get:Rule
     val benchmarkRule = BenchmarkRule()
 
